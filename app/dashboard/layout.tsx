45632a29b5aa52c5fcddcc460fdd87609284175c
Karl Tiama <karltiama@gmail.com>
2024-10-04 18:59:37 -0400

    fix: add bottom padding

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         <Sidebar />
         <div className="flex-1 flex flex-col overflow-hidden">
           <DashboardHeader />
-          <main className="flex-1 overflow-y-auto p-4">
+          <main className="flex-1 overflow-y-auto p-4 pb-24">
             {children}
           </main>
           <BottomNav />
